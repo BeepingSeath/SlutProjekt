f0c4784ca2aa99ee61c2089987d24e548b186eca
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-05-11 13:39:24 +0200

    Added Base for Enemy object

---
+++ src/Enemy.java
@@ -0,0 +1,29 @@
+public class Enemy {
+    private int DMG;
+    private int HP;
+    private int XP;
+
+    public void setDMG(int DMG) {
+        this.DMG = DMG;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public void setXP(int XP) {
+        this.XP = XP;
+    }
+
+    public int getDMG() {
+        return DMG;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public int getXP() {
+        return XP;
+    }
+}
